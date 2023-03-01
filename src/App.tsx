b7214d837f705c5088f2e678bfe57abd0dc1fe1c
furballer <lovecy@vip.qq.com>
2023-03-01 11:29:17 +0800

    v0.0.3

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload v0.0.1
+          Edit <code>src/App.tsx</code> and save to reload v0.0.3
         </p>
         <a
           className="App-link"
